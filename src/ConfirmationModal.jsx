19440e166ad8d036e1feb52e9378616e8557c9bc
Julio Alberto Hernandez Morales <julioalbertoh79@gmail.com>
2025-09-27 15:46:09 -0600

    modal editado

--- src/ConfirmationModal.jsx
+++ src/ConfirmationModal.jsx
@@ -5,7 +5,7 @@ const ConfirmationModal = ({ onConfirm, onCancel }) => {
   return (
     <div className={styles.modalBackdrop}>
       <div className={styles.modalContent}>
-        <p className={styles.modalMessage}>¿Estás seguro de que quieres eliminar este producto?</p>
+        <p className={styles.modalMessage}>¿Estás seguro de que quieres eliminar esta publicacion?</p>
         <div className={styles.modalActions}>
           <button className={styles.confirmButton} onClick={onConfirm}>Aceptar</button>
           <button className={styles.cancelButton} onClick={onCancel}>Cancelar</button>
